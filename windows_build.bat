f34af44a95d40acf1623a2591118ae40aa2c1cbe
Jim <jamesmbern@gmail.com>
2022-01-10 08:14:53 -0800

    Update windows_build.bat

--- windows_build.bat
+++ windows_build.bat
@@ -7,6 +7,6 @@ cls
 cl /nologo /I ext\glfw\include /I ext\glad\include ^
 -fp:except ^
 -Od ^
--Zi -GR- -EHa- -W4 -FC -wd4201 -wd4505 -wd4127 -wd4723 -wd4238 /MD %tmp%\main.cpp /link P:\ext\glfw\lib-vc2017\glfw3.lib OpenGL32.lib user32.lib gdi32.lib shell32.lib vcruntime.lib msvcrt.lib
+-Zi -GR- -EHa- -W4 -FC -wd4201 -wd4505 -wd4127 -wd4723 -wd4238 /MD %tmp%\main.cpp /link ..\windows_ext\glfw\lib-vc2017\glfw3.lib OpenGL32.lib user32.lib gdi32.lib shell32.lib vcruntime.lib msvcrt.lib
 
 popd
